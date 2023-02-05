02d96b99d999abb2176cbd8047fdd3f790eb767c
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-04 19:12:07 -0500

    Added basic home and notes page. Fetching record from pocketbase collection and display on notes page

--- notesapp/app/layout.tsx
+++ notesapp/app/layout.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-head-element */
-
+import Link from "next/link";
+import './globals.css'
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +9,16 @@ export default function RootLayout({
   return (
     <html>
       <head></head>
-      <body>{children}</body>
+      <body>
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/notes">Notes</Link>
+
+          </nav>
+          {children}
+        </main>
+        </body>
     </html>
   );
 }

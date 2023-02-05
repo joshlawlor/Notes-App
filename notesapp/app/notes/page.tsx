48184719876310245d27b7e8cbf6f1bd8bb4cd81
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-04 21:28:24 -0500

    Fixed seperate page routing error. Error.tsx needed to be a client component

--- notesapp/app/notes/page.tsx
+++ notesapp/app/notes/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import CreateNote from "./CreateNote";
 import styles from './Notes.module.css'
 
-
 async function getNotes() {
     const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30'
     , {cache: 'no-store'}
